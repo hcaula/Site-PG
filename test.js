05b9257e96ea58213e5699c07ff3f007eb55502c
hcaula <henrique.caula@hotmail.com>
2017-08-18 01:13:25 -0300

    Change command

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                fileDisplayArea.innerText = allText
+                console.log(allText);
             }
         }
     }
