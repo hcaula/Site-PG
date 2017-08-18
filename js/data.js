0caad278d4f6aa996e0c5033bbfa02a534ba9324
hcaula <henrique.caula@hotmail.com>
2017-08-18 07:43:09 -0300

    Formating using the data on the data.txt file

--- js/data.js
+++ js/data.js
@@ -1,1 +1,12 @@
+var horario = document.getElementById('horario');
+var sala = document.getElementById('local');
+var links = document.getElementById('links');
 
+horario.innerHTML = data.horario;
+sala.innerHTML = data.sala;
+
+links.innerHTML="<ul>"
+data.links.forEach(function(link){
+  links.innerHTML += "<li>"+link+"</li>";
+});
+links.innerHTML+="</ul>";
