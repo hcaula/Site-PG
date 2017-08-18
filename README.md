0bf4fd879ab5a85b278ace7d86329cbb9656f4e5
hcaula <henrique.caula@hotmail.com>
2017-08-18 02:28:46 -0300

    Add read file and create a data object

---
+++ README.md
@@ -0,0 +1,52 @@
+# Site do curso de Processamento Gráfico - IF680
+
+## Instruções para monitores
+Para facilitar a atualização da página semestral, existe um arquivo ```data.txt``` com as informações
+atualizadas da cadeira. A cada renovação de período, basta atualizar este arquivo e o site
+estará em dia.
+
+A fim de evitar erros de entrada no arquivo, siga o seguinte formato:
+
+```
+Período
+Professor
+Horário das aulas
+Sala
+Google Drive
+Grupo do Facebook
+Número de monitores
+(para cada monitor, faça)
+Nome do monitor
+Link do Facebook
+Caminho da imagem (em relação à root do projeto)
+```
+
+## Instruções para testes locais
+Navegadores normalmente não permitem acessos à arquivos locais por motivos de segurança.
+Uma alternativa é desbloquear essa restrições do seu navegador, porém, isso deixaria seu
+computador propenso à ataques.<br />
+Portanto, é preferível rodar um pequeno servidor para testar o HTML localmente. A forma
+mais simples é utilizar o módulo já instalado com o Python, o Simple HTTP Server.
+
+- Windows (não testado)
+
+```
+cd [diretorio do projeto]
+python -m http.server
+```
+
+- Linux/Mac
+
+```
+cd [diretorio do projeto]
+python -m SimpleHTTPServer
+```
+
+### Curso
+Universidade Federal de Pernambuco <br />
+Centro de Informática <br />
+Gradução em Ciência da Computação <br />
+Processamento Gráfico
+
+### Autor
+- Luiz Henrique Tavares Caúla | lhtc@cin.ufpe.br
---
+++ readFile.js
@@ -0,0 +1,62 @@
+var file, periodo, horario, sala, drive, facebook;
+var monitores = [];
+var data = {};
+
+function readTextFile(file) {
+
+  /* Crie o objeto XMLHTttpRequest */
+  var rawFile = new XMLHttpRequest();
+
+  /* Especifique o tipo de requisição e seus atributos */
+  rawFile.open("GET", file, false);
+
+  /* Chame essa função sempre que o estado da requisição HTTP mudar */
+  rawFile.onreadystatechange = function () {
+
+    /* Estado 4 significa "requisição concluída e a resposta está pronta" */
+    if(rawFile.readyState === 4) {
+
+      /* Estados comuns do HTTP */
+      if(rawFile.status === 200 || rawFile.status == 0) {
+
+        /* Pegue o resultado da requisição */
+        file = rawFile.responseText;
+
+        /* Colete os dados do data.txt */
+        var split = file.split('\n');
+        periodo = split[0];
+        professor = split[1];
+        horario = split[2];
+        sala = split[3];
+        drive = split[4];
+        facebook = split[5];
+
+        /* Variáveis auxiliares */
+        var numMonitores = parseInt(split[6]);
+        var lastLine = 6;
+
+        /* Pegue os monitores */
+        for(var i=0; i < numMonitores; i++){
+          var monitor = {};
+          monitor.nome = split[lastLine+i*3+1];
+          monitor.face = split[lastLine+i*3+2];
+          monitor.img = split[lastLine+i*3+3]
+          monitores.push(monitor);
+        }
+
+        /* Crie um objeto data para mais fácil manipulação */
+        data = {
+          periodo: periodo,
+          horario: horario,
+          sala: sala,
+          drive: drive,
+          facebook: facebook,
+          monitores: monitores
+        }
+      }
+    }
+  }
+  rawFile.send(null);
+}
+
+readTextFile("data.txt");
