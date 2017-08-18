126009d456435483063800294f56787942a59fe3
hcaula <henrique.caula@hotmail.com>
2017-08-18 05:24:20 -0300

    Pre-loading complete

--- js/aux.js
+++ js/aux.js
@@ -1,7 +1,10 @@
 var body = document.getElementById('body');
 var html = body.innerHTML;
 body.innerHTML =
-'<div style="height:100vh; width: 100vw; background-color: black"> <h1 style="text-align:center"> Carregando <i class="fa fa-circle-o-notch fa-spin"></i></h1></div>'
+'<div style="height: 100vh; width: 100vw; background-color: white;"> Carregando <i class="fa fa-circle-o-notch fa-spin"></i></div>'
+
+console.log(body.style);
+
 
 body.onload = function(){
   body.innerHTML = html;
