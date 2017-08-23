82d4c381759784dbc495dee0e594960ae55c9418
hcaula <henrique.caula@hotmail.com>
2017-08-23 15:59:04 -0300

    Double encryption

--- js/readFile.js
+++ js/readFile.js
@@ -2,13 +2,12 @@ var file, periodo, horario, sala, professor;
 var monitores = [], links = [], referencias = [];
 var data = {};
 var cronograma = {};
+var s = CryptoJS.AES.decrypt("U2FsdGVkX1/SgulY3AZ2DFJiaOiU7YVcDrDI7rJanqA=", "Secs Mess");
 
 /* Função para requisitar JSON (caledário) */
 var requestJSON = function(){
   var xhs = new XMLHttpRequest();
-  var s = CryptoJS.AES.decrypt("U2FsdGVkX1/SgulY3AZ2DFJiaOiU7YVcDrDI7rJanqA=", "Secs Mess");
-  var h = CryptoJS.AES.decrypt("U2FsdGVkX19UiRbhz+Q8AEfc70LcAri3i2HJDYneaPkBgsXSnEamiIfKX50KF993/rcywPINFt1WnqBm+wDbBwacJiSNHeq0Sy9qzP7aT30jS6QL2XPl2NognTnh63MN", s.toString(CryptoJS.enc.Utf8));
-  xhs.open("GET", h.toString(CryptoJS.enc.Utf8), false);
+  xhs.open("GET", CryptoJS.AES.decrypt("U2FsdGVkX19UiRbhz+Q8AEfc70LcAri3i2HJDYneaPkBgsXSnEamiIfKX50KF993/rcywPINFt1WnqBm+wDbBwacJiSNHeq0Sy9qzP7aT30jS6QL2XPl2NognTnh63MN", s.toString(CryptoJS.enc.Utf8)).toString(CryptoJS.enc.Utf8), false);
   xhs.onreadystatechange = function () {
     if(xhs.readyState === 4) {
       if(xhs.status === 200 || xhs.status == 0) {
@@ -23,8 +22,7 @@ var requestJSON = function(){
 
 var requestCalendar = function(){
   var xhs = new XMLHttpRequest();
-  var h = CryptoJS.AES.decrypt("U2FsdGVkX18rD6yVPykv0Hfwe2c4dTe/YS6Obbmae//tohdERrhyiaCQ2jUcCh2A7xj34dP+R1H231TLoLHecCUZH+RNkvBt1Mq1qmgXk1E=", "Secret Message");
-  xhs.open('GET', h.toString(CryptoJS.enc.Utf8), false);
+  xhs.open('GET', CryptoJS.AES.decrypt("U2FsdGVkX18rD6yVPykv0Hfwe2c4dTe/YS6Obbmae//tohdERrhyiaCQ2jUcCh2A7xj34dP+R1H231TLoLHecCUZH+RNkvBt1Mq1qmgXk1E=", s.toString(CryptoJS.enc.Utf8)).toString(CryptoJS.enc.Utf8), false);
   xhs.onreadystatechange = function() {
     if(xhs.readyState === 4) {
       if(xhs.status === 200 || xhs.status == 0) {
