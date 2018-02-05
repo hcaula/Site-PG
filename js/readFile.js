7a67cb9163786c4cdbaa108c5422971380f12f86
hcaula <henrique.caula@hotmail.com>
2018-02-05 00:57:19 -0300

    Change gists links

--- js/readFile.js
+++ js/readFile.js
@@ -3,10 +3,11 @@ var monitores = [], links = [], referencias = [];
 var data = {};
 var cronograma = {};
 
-/* Função para requisitar JSON (caledário) */
+/* Função para requisitar siteInfo */
 var requestJSON = function(){
   var xhs = new XMLHttpRequest();
-  xhs.open("GET", "https://api.github.com/gists/f41afdcb2da2801ee7f0f21c5a4a2aba", false);
+  xhs.open("GET", "https://api.github.com/gists/88537bf7372495ced9bbb854d24ccef2", false);
+  // xhs.open("GET", "https://api.github.com/gists/f41afdcb2da2801ee7f0f21c5a4a2aba", false);
   xhs.onreadystatechange = function () {
     if(xhs.readyState === 4) {
       if(xhs.status === 200 || xhs.status == 0) {
@@ -20,9 +21,10 @@ var requestJSON = function(){
   xhs.send(null);
 }
 
+/* Função para requisitar cronograma */
 var requestCalendar = function(){
   var xhs = new XMLHttpRequest();
-  xhs.open('GET', "https://api.github.com/gists/250348f30528f1861576a142c6f3e9fa", false);
+  xhs.open('GET', "https://api.github.com/gists/cb2372cf46da4a5b4b1d5a59478cea68", false);
   xhs.onreadystatechange = function() {
     if(xhs.readyState === 4) {
       if(xhs.status === 200 || xhs.status == 0) {
